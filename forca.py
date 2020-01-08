c50acb6826d7d08bb082f92cd8a1d26be4bf334a
Rafael Almeida <supremotop10@gmail.com>
2020-01-08 17:02:32 -0300

    ADD mais arquivos da aula

--- forca.py
+++ forca.py
@@ -3,6 +3,22 @@ def jogar():
     print("***Bem vindo ao jogo de forca!***")
     print("*********************************")
 
+    palavra_secreta = "banana"
+    enforcou = False
+    acertou = False
+
+    #enquanto não enforcou e não acertou
+    while(not enforcou and not acertou):
+        # lower() converte toda str para minusculo.
+        # upper() para maiusculo.
+        # strip() retira os respaços digitados em uma string.
+        chute = input("Qual letra?").lower().strip()
+
+        index = 0
+        for letra in palavra_secreta:
+            if(chute == letra):
+                print("Encontrei a letra {} na posição {}".format(chute, index))
+            index += 1
     print("Fim do jogo")
 if(__name__ == "__main__"):
     jogar()
