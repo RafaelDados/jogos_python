8eb2faf3eab60b7198b8b6190932bb5bed469df6
Rafael Almeida <supremotop10@gmail.com>
2020-01-09 15:38:23 -0300

    ADD lista

--- forca.py
+++ forca.py
@@ -4,21 +4,29 @@ def jogar():
     print("*********************************")
 
     palavra_secreta = "banana"
+    letras_acertadas = ["_", "_","_","_","_","_"]
+
+
     enforcou = False
     acertou = False
+
+    print(letras_acertadas)
 
     #enquanto não enforcou e não acertou
     while(not enforcou and not acertou):
         # lower() converte toda str para minusculo.
         # upper() para maiusculo.
         # strip() retira os respaços digitados em uma string.
-        chute = input("Qual letra?").lower().strip()
+        chute = input("Qual letra?").strip()
 
         index = 0
         for letra in palavra_secreta:
-            if(chute == letra):
-                print("Encontrei a letra {} na posição {}".format(chute, index))
+            if(chute.upper() == letra.upper()):
+                letras_acertadas[index] = letra.upper()
+                letras_faltando = str(letras_acertadas.count('_'))
             index += 1
+        print(letras_acertadas)
+        print('Ainda faltam acertar {} letras'.format(letras_faltando))
     print("Fim do jogo")
 if(__name__ == "__main__"):
     jogar()
