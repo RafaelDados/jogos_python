cffed82e0808aa9d0c6198de1ce9e87c2dda9bee
Rafael Almeida <supremotop10@gmail.com>
2020-01-09 16:38:53 -0300

    ADD algumas funcionalidades além das do curso

--- forca.py
+++ forca.py
@@ -3,12 +3,13 @@ def jogar():
     print("***Bem vindo ao jogo de forca!***")
     print("*********************************")
 
-    palavra_secreta = "banana"
+    palavra_secreta = "banana".upper()
     letras_acertadas = ["_", "_","_","_","_","_"]
-
+    letras_faltando = len(palavra_secreta)
 
     enforcou = False
     acertou = False
+    tentativas = 0
 
     print(letras_acertadas)
 
@@ -17,16 +18,31 @@ def jogar():
         # lower() converte toda str para minusculo.
         # upper() para maiusculo.
         # strip() retira os respaços digitados em uma string.
-        chute = input("Qual letra?").strip()
+        chute = input("Qual letra?").strip().upper()
 
-        index = 0
-        for letra in palavra_secreta:
-            if(chute.upper() == letra.upper()):
-                letras_acertadas[index] = letra.upper()
-                letras_faltando = str(letras_acertadas.count('_'))
-            index += 1
+        if(chute in palavra_secreta):
+            index = 0
+            for letra in palavra_secreta:
+                if(chute.upper() == letra.upper()):
+                    letras_acertadas[index] = letra.upper()
+                    letras_faltando = str(letras_acertadas.count('_'))
+                index += 1
+        else:
+            tentativas += 1
+
+        #Caso as tentativas chegue ao número 6 a váriavel enforcou será False
+        enforcou = tentativas == 6
+
+        acertou = "_" not in letras_acertadas
+        if(acertou != True):
+            print('Ainda faltam acertar {} letras'.format(letras_faltando))
+        if(acertou):
+            print("Você ganhou!!")
+        else:
+            print("Você perdeu!!")
         print(letras_acertadas)
-        print('Ainda faltam acertar {} letras'.format(letras_faltando))
+
     print("Fim do jogo")
+
 if(__name__ == "__main__"):
     jogar()
